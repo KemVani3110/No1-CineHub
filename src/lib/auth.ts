b33f03bdd3c16de0909e0fa213d91ca30b75dff1
MrKemVani <chuminhkhoi3110@gmail.com>
2025-06-11 15:22:54 +0700

    fix auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -35,7 +35,7 @@ declare module 'next-auth/jwt' {
 if (!getApps().length) {
   initializeApp({
     credential: cert({
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     }),
